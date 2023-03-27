5b0bef91ba033f765c72a22f722b914fd2893afb
Paraschiv Olivia 60085 <g60085@APP.PDG.INTRANET.ESI-BRU.BE>
2023-03-27 09:36:39 +0200

    classe Tile et Record Tile_Bis

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/App.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/App.java
@@ -1,11 +1,33 @@
 package g60085.qwirkle;
 
 import g60085.qwirkle.model.Color;
+import g60085.qwirkle.model.Shape;
+import g60085.qwirkle.model.Tile;
+import g60085.qwirkle.model.Tile_Bis;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("DEV2: jeu de QWIRKLE");
-        //values(); //tableau reprenant toutes le couleurs
+     /*
+        Color color = new Color();
+        color.values(); //tableau reprenant toutes le couleurs de l'enumeration
+     */
+
+     /*
+        Tile tile = new Tile(Color.RED, Shape.SQUARE);
+        System.out.println(tile); //Grace à la methode toString
+        //c'est ici qu'on donnera les vrai valeur des shape et color
+     */
+    /*
+        Tile_Bis tile_bis = new Tile_Bis(Color.BLUE, Shape.CROSS);
+        System.out.println(tile_bis); //Grace à la methode toString;
+        mais par defaut record fait aussi la methode ToString qu'on a crée
+        tile_bis.color(); //getter que le record a cree lui meme;
+     */
+
+
+
+
     }
 
 }
