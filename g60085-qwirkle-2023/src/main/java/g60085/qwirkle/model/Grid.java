63dfff95dbe03994233f88b2851c2b087d332caf
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-03 14:42:47 +0200

    grid et grid test pour deuxieme methode add

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Grid.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Grid.java
@@ -82,19 +82,56 @@ public class Grid {
     }
 
 
-//ajoute une tuile à une certaine position en respectant les règles du jeu
-public void add(int row, int col, Tile tile) throws QwirkleException{
-        if(row>91 || row <0 ||  col>91 || col<0){
+    //ajoute une tuile à une certaine position en respectant les règles du jeu
+    public void add(int row, int col, Tile tile) throws QwirkleException {
+        if (row > 91 || row < 0 || col > 91 || col < 0) {
             throw new NullPointerException();
         }
-        if(row==45 && col==45){
+        if (row == 45 && col == 45) {
             throw new QwirkleException("at this position you must call the firtAdd method");
         }
-        if(this.tiles[row][col]!= null){
+        if (this.tiles[row][col] != null) {
             throw new QwirkleException("there is already a tile at this position");
         }
-        this.tiles[row][col]=tile;
-}
+        this.tiles[row][col] = tile;
+    }
+    public void add(int row, int col, Direction d, Tile... line){
+        if (row > 91 || row < 0 || col > 91 || col < 0) {
+            throw new NullPointerException();
+        }
+        if (row == 45 && col == 45) {
+            throw new QwirkleException("at this position you must call the firtAdd method");
+        }
+        if (this.tiles[row][col] != null) {
+            throw new QwirkleException("there is already a tile at this position");
+        }
+        if (line.length > 6) {
+            throw new QwirkleException("The length is above the limit");
+        }
+        if (line.length > 1) {
+            if (!sameColor(line)) {
+                if (!sameShape(line)) {
+                    throw new QwirkleException("Tiles must be of the same color or shape");
+                }
+            }
+            if (sameTile(line)) {
+                throw new QwirkleException("Cannot have the same tile twice");
+            }
+        }
+        if(row+line.length >91 || row- line.length <0 || col+ line.length>91 ||col- line.length<0){
+            throw new QwirkleException("Cannot have the same tile twice");
+        }
 
+
+        this.tiles[row][col] = line[0];
+        for (int i = 1; i< line.length; i++){
+            row = row + d.getDeltaRow();
+            col = col+d.getDeltaCol();
+            if(this.tiles[row][col]!= null){
+                throw new QwirkleException("there is already a tile at this position");
+            }
+            this.tiles[row][col] =line[i];
+        }
+    }
 
 }
--- g60085-qwirkle-2023/src/test/java/g60085/qwirkle/model/GridTest.java
+++ g60085-qwirkle-2023/src/test/java/g60085/qwirkle/model/GridTest.java
@@ -26,30 +26,6 @@ class GridTest {
     }
 
     @Test
-    void rules_sonia_a() {
-        var t1 = new Tile(RED, ROUND);
-        var t2 = new Tile(RED, DIAMOND);
-        var t3 = new Tile(RED, PLUS);
-        grid.firstAdd(UP, t1, t2, t3);
-        assertEquals(t1, grid.get(45, 45));
-        assertEquals(t2, grid.get(45 - 1, 45));
-        assertEquals(t3, grid.get(45 - 2, 45));
-    }
-
-    @Test
-    void rules_sonia_a_adapted_to_fail() {
-        var t1 = new Tile(RED, ROUND);
-        var t2 = new Tile(RED, DIAMOND);
-        var t3 = new Tile(RED, DIAMOND);
-        assertThrows(QwirkleException.class, () -> {
-            grid.firstAdd(UP, t1, t2, t3);
-        });
-        assertNull(grid.get(45, 45));
-        assertNull(grid.get(45 - 1, 45));
-        assertNull(grid.get(45 - 2, 45));
-    }
-
-    @Test
     void firstAdd_cannot_be_called_twice() {
         Tile redcross = new Tile(RED, CROSS);
         Tile reddiamond = new Tile(RED, DIAMOND);
@@ -57,15 +33,12 @@ class GridTest {
         assertThrows(QwirkleException.class, () -> grid.firstAdd(DOWN, redcross, reddiamond));
     }
 
-
     //add
-
     @Test
     void firstAdd_must_be_called_first_simple() {
         Tile redcross = new Tile(RED, CROSS);
         assertThrows(QwirkleException.class, () -> grid.add(45, 45, redcross));
     }
-
 
        /* @Test
         @DisplayName("get outside the grid should return null, not throw")
@@ -88,5 +61,114 @@ class GridTest {
         assertThrows(QwirkleException.class, () -> grid.add(3,3, redsquare));
     }
 
+    //add several tiles
+    @Test
+    void add_several_tiles() {
+        var t1 = new Tile(RED, ROUND);
+        var t2 = new Tile(RED, DIAMOND);
+        var t3 = new Tile(RED, PLUS);
+        grid.add(3,3, UP, t1, t2, t3);
+        assertEquals(t1, grid.get(3, 3));
+        assertEquals(t2, grid.get(2, 3));
+        assertEquals(t3, grid.get(1, 3));
+    }
+    @Test
+    void add_7_tiles() {
+        var t1 = new Tile(RED, ROUND);
+        var t2 = new Tile(RED, DIAMOND);
+        var t3 = new Tile(RED, PLUS);
+        var t4 = new Tile(RED, SQUARE);
+        var t5 = new Tile(RED, ROUND);
+        var t6 = new Tile(RED, CROSS);
+        var t7 = new Tile(RED, CROSS);
+        grid.add(3,3, UP, t1, t2, t3);
 
+    }
+
+
+    //general cases
+    @Test
+    void rules_sonia_a() {
+        var t1 = new Tile(RED, ROUND);
+        var t2 = new Tile(RED, DIAMOND);
+        var t3 = new Tile(RED, PLUS);
+        grid.firstAdd(UP, t1, t2, t3);
+        assertEquals(t1, grid.get(45, 45));
+        assertEquals(t2, grid.get(44, 45));
+        assertEquals(t3, grid.get(43, 45));
+    }
+    @Test
+    void rules_sonia_a_adapted_to_fail() {
+        var t1 = new Tile(RED, ROUND);
+        var t2 = new Tile(RED, DIAMOND);
+        var t3 = new Tile(RED, DIAMOND);
+        assertThrows(QwirkleException.class, ()->{
+            grid.firstAdd(UP, t1, t2, t3);
+        });
+        assertNull(grid.get(45,45));
+        assertNull(grid.get(44,45));
+        assertNull(grid.get(43,45));
+    }
+
+    @Test
+    void rules_cédric_b() {
+        var t1 = new Tile(RED, SQUARE);
+        var t2 = new Tile(BLUE, SQUARE);
+        var t3 = new Tile(PURPLE, SQUARE);
+        grid.add(46,45,RIGHT, t1, t2, t3);
+        assertEquals(t1, grid.get(46, 45));
+        assertEquals(t2, grid.get(46, 46));
+        assertEquals(t3, grid.get(46, 47));
+    }
+    @Test
+    void rules_cédric_b_adapted_to_fail() {
+        var t1 = new Tile(RED, SQUARE);
+        var t2 = new Tile(BLUE, SQUARE);
+        var t3 = new Tile(PURPLE, SQUARE);
+        assertThrows(QwirkleException.class, ()->{
+            grid.add(45,45,RIGHT, t1, t2, t3);
+        });
+        assertNull(grid.get(45, 45));
+        assertNull(grid.get(45, 46));
+        assertNull(grid.get(45, 47));
+    }
+
+    @Test
+    void rules_Elvire_c() {
+        var t1 = new Tile(BLUE, ROUND);
+        grid.add(45,46, t1);
+        assertEquals(t1, grid.get(45, 46));
+    }
+    @Test
+    void rules_Elvire_c_adapted_to_fail() {
+        var t1 = new Tile(RED, SQUARE);
+        grid.add(40, 40, t1);
+        var t2 = new Tile(BLUE, ROUND);
+        assertThrows(QwirkleException.class, ()->{
+            grid.add(40,40, t2);
+        });
+        assertEquals(t1, grid.get(40, 40));
+    }
+    @Test
+    void rules_Vincent_d() {
+        var t1 = new Tile(GREEN, CROSS);
+        var t2 = new Tile(GREEN, DIAMOND);
+        grid.add(43,44,DOWN, t1, t2);
+        assertEquals(t1, grid.get(43, 44));
+        assertEquals(t2, grid.get(44, 44));
+    }
+    @Test
+    void rules_Vincent_d_adapted_to_fail() {
+        var t1 = new Tile(ORANGE, ROUND);
+        var t2 = new Tile(ORANGE, SQUARE);
+        grid.add(44, 44, DOWN, t1, t2);
+        var t3 = new Tile(GREEN, CROSS);
+        var t4 = new Tile(GREEN, DIAMOND);
+        assertThrows(QwirkleException.class, ()->{
+            grid.add(43,44,DOWN, t3, t4);
+        });
+        assertEquals(t1, grid.get(44, 44));
+        assertEquals(t2, grid.get(45, 44));
+        assertEquals(t3, grid.get(43,44));
+    }
 }
