856488dc0301abf15aeb68d5b7355ecb1e5449fb
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-06 16:40:03 +0200

    ajout de la methode (changeCurrentPlayer) et ajout dans toutes les methodes play, la fonctionnalité (remove et refill)

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Game.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Game.java
@@ -16,6 +16,11 @@ public class Game {
         this.currentPlayer = 0;
     }
 
+    //pas sure
+    public Player[] getPlayers() {
+        return this.players;
+    }
+
     public void first(Direction d, int... is) {
         Player firstPlayer = this.players[this.currentPlayer];
         Tile[] tiles = new Tile[is.length];
@@ -23,14 +28,20 @@ public class Game {
             tiles[i] = firstPlayer.getTiles().get(is[i]);
         }
         this.grid.firstAdd(d, tiles);
-        this.currentPlayer++;
+        //System.out.println(getCurrentPlayerHand());
+        firstPlayer.remove(tiles);
+        firstPlayer.refill();
+        //System.out.println(getCurrentPlayerHand());
+        changeCurrentPlayer();
     }
 
     public void play(int row, int col, int index) {
         Player player = this.players[this.currentPlayer];
         Tile tile = player.getTiles().get(index);
         this.grid.add(row, col, tile);
-        this.currentPlayer++;
+        player.remove(tile);
+        player.refill();
+        changeCurrentPlayer();
     }
 
     public void play(int row, int col, Direction d, int... indexes) {
@@ -40,42 +51,66 @@ public class Game {
             tiles[i] = player.getTiles().get(indexes[i]);
         }
         this.grid.add(row, col, d, tiles);
-        this.currentPlayer++;
+        player.remove(tiles);
+        player.refill();
+        changeCurrentPlayer();
     }
 
     public void play(int... is) {
         Player player = this.players[this.currentPlayer];
         TileAtPosition[] tiles = new TileAtPosition[is.length / 3];
+        Tile[] tilesToRemove = new Tile[is.length / 3];
         int tilesIndex = 0;
-            for (int i = 0; i < is.length; i = i + 3) {
-                int row = is[i];
-                int col = is[i + 1];
-                Tile tile = player.getTiles().get(is[i + 2]);
-                tiles[tilesIndex] = new TileAtPosition(row, col, tile);
-                tilesIndex++;
-            }
+        for (int i = 0; i < is.length; i = i + 3) {
+            int row = is[i];
+            int col = is[i + 1];
+            Tile tile = player.getTiles().get(is[i + 2]);
+            tiles[tilesIndex] = new TileAtPosition(row, col, tile);
+            tilesToRemove[tilesIndex] = tile;
+            tilesIndex++;
+        }
         this.grid.add(tiles);
-        this.currentPlayer++;
+        player.remove(tilesToRemove);
+        player.refill();
+        changeCurrentPlayer();
     }
 
     public String getCurrentPlayerName() {
         return this.players[this.currentPlayer].getName();
     }
 
-    public List<Tile> getCurrentPlayerHand(){
+    public List<Tile> getCurrentPlayerHand() {
+
         return this.players[this.currentPlayer].getTiles();
     }
 
-    public void pass()throws QwirkleException{
+    public void pass() throws QwirkleException {
         throw new QwirkleException("Coup impossible");
     }
 
-    public GridView getGrid(){
+    public GridView getGrid() {
         GridView gridView = new GridView(this.grid);
         return gridView;
     }
 
+    //pas sure
+    public void setCurrentPlayer(String name) {
+        int i = 0;
+        while (i < this.players.length && !this.players[i].getName().equals(name)) {
+            i= i+1;
+        }
+        if (i == this.players.length) {
+            System.out.println("This name doesn't exist, try again! : ");
+        } else {
+            this.currentPlayer = i;
+        }
+    }
 
-
+    public void changeCurrentPlayer(){
+        this.currentPlayer++;
+        if(this.currentPlayer == this.players.length){
+            this.currentPlayer = 0;
+        }
+    }
 
 }
