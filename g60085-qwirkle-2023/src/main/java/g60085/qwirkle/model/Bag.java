c2bf15a4ccd5c1daf7dbedb9f91c2e8311002a55
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-13 20:30:26 +0200

    javadoc de la classe Bag

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Bag.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Bag.java
@@ -5,13 +5,16 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Bag represents the bag of tiles
+ * Bag represents the bag of tiles in the game.
  */
 public class Bag {
-    private List<Tile> tiles;
+    private final List<Tile> tiles;
     private static Bag instance;
 
-    //constructeur
+    /**
+     * Creates an instance of the type Bag by creating 108 tiles.
+     * Since there are 6 colors and 6 shapes, it initializes 36 different tiles with 3 copies of each tile.
+     */
     private Bag() {
         this.tiles = new ArrayList<>();
         for (int i = 0; i < Color.values().length; i++) {
@@ -24,41 +27,64 @@ public class Bag {
         Collections.shuffle(tiles);
     }
 
-
+    /**
+     * Instantiate a single tile bag using the design pattern Singleton.
+     *
+     * @return the only instance of type Bag.
+     */
     public static Bag getInstance() {
-        if(instance == null){
+        if (instance == null) {
             instance = new Bag();
         }
         return instance;
     }
 
+    /**
+     * Allows access to tiles from the bag in other classes.
+     *
+     * @param n number of tiles requested.
+     * @return an array with the requested tiles, null if the bag is empty or
+     * an array of remaining tiles if there are not enough left.
+     */
     public Tile[] getRandomTiles(int n) {
         if (this.tiles == null) {
             return null;
         }
+        Tile[] randomTiles;
         if (n <= this.tiles.size()) {
-            Tile[] randomTiles = new Tile[n];
-            for (int i = 0; i < n; i++) {
-                randomTiles[i] = tiles.remove(tiles.size() - 1); //retourne l'élément supprimé
+            randomTiles = new Tile[n];
+            for (int i = 0; i < randomTiles.length; i++) {
+                randomTiles[i] = tiles.remove(tiles.size() - 1); //returns the removed element
             }
-            return randomTiles;
         } else {
-            Tile[] randomTiles = new Tile[tiles.size()];
+            randomTiles = new Tile[tiles.size()];
             for (int i = 0; i < randomTiles.length; i++) {
                 randomTiles[i] = tiles.remove(tiles.size() - 1);
             }
-            return randomTiles;
         }
+        return randomTiles;
     }
 
-    @Override
-    public String toString() {
-        return "tiles = " + this.tiles;
-    }
+    //For tests
 
+    /**
+     * Gives access to the size of the tiles attribute.
+     *
+     * @return the size of the bag of tiles.
+     */
     public int size() {
         return this.tiles.size();
     }
 
+    //not requested in the project
 
+    /**
+     * Allows to see the tiles of the bag as text.
+     *
+     * @return a string representation of the object Bag.
+     */
+    @Override
+    public String toString() {
+        return "tiles = " + this.tiles;
+    }
 }
