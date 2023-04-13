790a122d5ab288aa2a8a812fdd7d7a559a798fc9
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-13 21:08:31 +0200

    Javadoc pour l'énumération Direction

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Direction.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Direction.java
@@ -1,41 +1,59 @@
 package g60085.qwirkle.model;
 
+/**
+ * Direction represents the direction in which multiple tiles are placed at once.
+ */
 public enum Direction {
     UP(-1, 0),
     DOWN(1, 0),
     LEFT(0, -1),
     RIGHT(0, 1);
 
-    private int deltaRow;
-    private int deltaCol;
 
-    //constructeur
-    Direction(int deltaRow, int deltaCol) { //il faut donner des valeurs aux énumerations une fois qu'on met un constructeur
+    private int deltaCol;
+    private int deltaRow;
+
+
+    /**
+     * Initializes the values of the Direction enumeration with the values passed in parameters.
+     *
+     * @param deltaRow tiles placement line index.
+     * @param deltaCol tiles placement column index.
+     */
+    Direction(int deltaRow, int deltaCol) { 
         this.deltaRow = deltaRow;
         this.deltaCol = deltaCol;
     }
 
-    //getter
+    /**
+     * Gives access to the deltaRow attribute.
+     *
+     * @return the deltaRow attribute.
+     */
     public int getDeltaRow() {
         return this.deltaRow;
     }
 
+    /**
+     * Gives access to the deltaCol attribute.
+     *
+     * @return the deltaCol attribute.
+     */
     public int getDeltaCol() {
         return this.deltaCol;
     }
 
+    /**
+     * @return the opposite direction of the current one.
+     */
+
     public Direction opposite() {
-        switch (this) {
-            case UP:
-                return DOWN;
-            case DOWN:
-                return UP;
-            case LEFT:
-                return RIGHT;
-            case RIGHT:
-                return LEFT;
-            default:
-                throw new IllegalArgumentException("pas une direction");
-        }
+        return switch (this) {
+            case UP -> DOWN;
+            case DOWN -> UP;
+            case LEFT -> RIGHT;
+            case RIGHT -> LEFT;
+            default -> throw new IllegalArgumentException("Not a direction");
+        };
     }
 }
