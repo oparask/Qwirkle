90be20004cc740b702caf936ff0513f6f8e05f37
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-13 18:53:18 +0200

    javadoc de la classe Tile

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Tile.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Tile.java
@@ -1,11 +1,14 @@
 package g60085.qwirkle.model;
 
-//on a deja un constructeur et un getter
 /**
- * Tile represents a tile in the game
+ * Tile represents a tile in the game.
  */
 public record Tile(Color color, Shape shape) {
-
+    /**
+     * Allows to see the shape and color of a tile as text.
+     *
+     * @return a string representation of the object Tile.
+     */
     @Override
     public String toString() {
         return this.shape.name() + ":" + this.color.name(); //on peut checher le nom d'une enum
