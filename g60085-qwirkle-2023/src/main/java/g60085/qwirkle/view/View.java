f886bbf97b5da697ff683bd4ec1384c066724f29
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-29 22:11:25 +0200

    supression de certaines methodes

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/view/View.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/view/View.java
@@ -1,27 +1,31 @@
 package g60085.qwirkle.view;
 
-import g60085.qwirkle.model.*;
-import java.util.ArrayList;
+import g60085.qwirkle.model.Game;
+import g60085.qwirkle.model.GridView;
+import g60085.qwirkle.model.Tile;
+
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Contains only static methods to display different parts of the game in the console.
  */
 public class View {
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RED = "\033[38;5;196m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_YELLOW = "\u001B[93m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_ORANGE = "\u001B[38;5;208m";
 
     /**
      * Displays game title at the beginning.
      */
     public static void beginning() {
-        System.out.println("Q W I R K L E");
+        System.out.println(ANSI_CYAN + "  _ _ _ _ _ _ _");
+        System.out.println("| Q W I R K L E |");
+        System.out.println("  ------------- " + ANSI_RESET);
         System.out.println();
     }
 
@@ -45,7 +49,7 @@ public class View {
             case BLUE -> System.out.print(ANSI_BLUE + shape + ANSI_RESET);
             case RED -> System.out.print(ANSI_RED + shape + ANSI_RESET);
             case GREEN -> System.out.print(ANSI_GREEN + shape + ANSI_RESET);
-            case ORANGE -> System.out.print(ANSI_CYAN + shape + ANSI_RESET);
+            case ORANGE -> System.out.print(ANSI_ORANGE + shape + ANSI_RESET);
             case PURPLE -> System.out.print(ANSI_PURPLE + shape + ANSI_RESET);
             case YELLOW -> System.out.print(ANSI_YELLOW + shape + ANSI_RESET);
         }
@@ -57,9 +61,41 @@ public class View {
      * @param grid the grid view to display.
      */
     public static void display(GridView grid) {
-        for (int i = 37; i < 51; i++) {
+        int colLeft = 45;
+        int colRight = 45;
+        for (int i = 3; i < 87; i++) { //row
+            for (int j = 0; j < 45; j++) { //left col
+                if (grid.get(i, j) != null && j < colLeft) {
+                    colLeft = j;
+                    break;
+                }
+            }
+            for (int j = 86; j > 45; j--) { //right col
+                if (grid.get(i, j) != null && j > colRight) {
+                    colRight = j;
+                    break;
+                }
+            }
+        }
+        int rowUp = 45;
+        int rowDown = 45;
+        for (int j = colLeft; j <= colRight; j++) { //col
+            for (int i = 0; i < 45; i++) { //up row
+                if (grid.get(i, j) != null && i < rowUp) {
+                    rowUp = i;
+                    break;
+                }
+            }
+            for (int i = 86; i > 45; i--) {//down row
+                if (grid.get(i, j) != null && i > rowDown) {
+                    rowDown = i;
+                    break;
+                }
+            }
+        }
+        for (int i = rowUp - 3; i < rowDown + 4; i++) { //row
             System.out.print(i + " |");
-            for (int j = 43; j < 53; j++) {
+            for (int j = colLeft - 3; j < colRight + 4; j++) {
                 if (grid.get(i, j) == null) {
                     System.out.print("   ");
                 } else {
@@ -68,8 +104,8 @@ public class View {
             }
             System.out.println();
         }
-        System.out.print("    ");
-        for (int j = 43; j < 53; j++) {
+        System.out.print("    "); //col
+        for (int j = colLeft - 3; j < colRight + 4; j++) {
             System.out.print(j + " ");
         }
         System.out.println();
@@ -81,8 +117,8 @@ public class View {
      * @param game the Qwirkle game.
      */
     public static void displayPlayer(Game game) {
-        System.out.println(game.getCurrentPlayerName() + "! It's your turn!");
-        System.out.print("Your hand is : ");
+        System.out.println(ANSI_CYAN + game.getCurrentPlayerName() + ", It's your turn!");
+        System.out.print("Your hand is : " + ANSI_RESET);
         List<Tile> hand = game.getCurrentPlayerHand();
         for (Tile tile : hand) {
             ViewColorShape(tile);
@@ -94,16 +130,15 @@ public class View {
      * Displays Qwirkle game commands.
      */
     public static void displayHelp() {
-        System.out.println("Q W I R K L E");
-        System.out.println("Qwirkle command:");
-        System.out.println("- play 1 tile : o <row> <col> <i>");
-        System.out.println("- play line: l <row> <col> <direction> <i1> [<i2>]");
-        System.out.println("- play plic-ploc : m <row1> <col1> <i1> [<row2> <col2> <i2>]");
-        System.out.println("- play first : f <i1> [<i2>]");
-        System.out.println("- pass : p");
-        System.out.println("- quit : q");
-        System.out.println("    i : index in list of tiles");
-        System.out.println("    d : direction in l (left), r (right), u (up), d(down)");
+        System.out.println(ANSI_PURPLE + "Qwirkle command:\n"
+                + "- play 1 tile : o <row> <col> <i>\n"
+                + "- play line: l <row> <col> <direction> <i1> [<i2>]\n"
+                + "- play plic-ploc : m <row1> <col1> <i1> [<row2> <col2> <i2>]\n"
+                + "- play first : f <i1> [<i2>]\n"
+                + "- pass : p\n"
+                + "- quit : q\n"
+                + "    i : index in list of tiles\n"
+                + "    d : direction in l (left), r (right), u (up), d(down)\n" + ANSI_RESET);
     }
 
     /**
@@ -112,272 +147,17 @@ public class View {
      * @param message the error message.
      */
     public static void displayError(String message) {
-        System.out.println(message);
-    }
-
-    /**
-     * Robust reading of an integer.
-     *
-     * @return an integer.
-     */
-    public static int robustReadingInt() {
-        Scanner keyboard = new Scanner(System.in);
-        while (!keyboard.hasNextInt()) {
-            System.out.println("Invalid number, try again!: ");
-            keyboard.next();
-        }
-        return keyboard.nextInt();
-    }
-
-    /**
-     * Robust reading of the number of players.
-     * Checks that this number respects the rules of the game.
-     *
-     * @return a valid number of players.
-     */
-    public static int robustReadingPlayers() {
-        int number = robustReadingInt();
-        while (number < 2 || number > 4) {
-            System.out.println("The number of players must be between 2 and 4, try again! : ");
-            number = robustReadingInt();
-        }
-        return number;
-    }
-
-    /**
-     * Robust reading of a String.
-     *
-     * @return a String.
-     */
-    public static String robustReadingString() {
-        Scanner keyboard = new Scanner(System.in);
-        while (!keyboard.hasNextLine()) {
-            System.out.println("Invalid String, try again! : ");
-            keyboard.next();
-        }
-        return keyboard.nextLine();
-    }
-
-    /**
-     * Robust reading of a direction.
-     *
-     * @return a valid direction.
-     */
-    public static String robustReadingDirection() {
-        Scanner keyboard = new Scanner(System.in);
-        String direction = robustReadingString();
-        while ((!direction.equals("l")) && (!direction.equals("u")) && (!direction.equals("r")) && (!direction.equals("d"))) {
-            System.out.print("The direction letter is invalid, do you need help (y or n) ? : ");
-            String help = keyboard.nextLine();
-            if (help.equals("y")) {
-                View.displayHelp();
-            }
-            direction = robustReadingString();
-        }
-        return direction;
-    }
-
-    /**
-     * Robust reading of an index.
-     * Checks that the index respects the rules of the game.
-     *
-     * @return a valid index.
-     */
-    public static int robustReadingIndexes() {
-        System.out.println("Enter the index of the tile that you want to play: ");
-        int index = robustReadingInt();
-        while (index < 0 || index > 5) {
-            System.out.println("The index must be between 0 and 5 , try again! : ");
-            index = robustReadingInt();
-        }
-        return index;
-    }
-
-    /**
-     * Asks for the number of players.
-     *
-     * @return the number of players.
-     */
-    public static int nbPlayers() {
-        System.out.print("Enter the number of players: ");
-        return robustReadingPlayers();
-    }
-
-    /**
-     * Asks for the names of the players.
-     *
-     * @param nbPlayers number of players.
-     * @return an array list containing the names of the players.
-     */
-    public static List<Player> namePlayers(int nbPlayers) {
-        List<Player> playersList = new ArrayList<>();
-        for (int i = 0; i < nbPlayers; i++) {
-            System.out.print("Enter the name of the player " + (i + 1) + ": ");
-            String name = robustReadingString();
-            Player player = new Player(name);
-            playersList.add(player);
-        }
+        System.out.println(ANSI_ORANGE + "Impossible move!");
+        System.out.println("Reason: " + message + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Try again!" + ANSI_RESET);
         System.out.println();
-        System.out.print("The players of this Qwirkle game are: ");
-        for (Player player : playersList) {
-            System.out.print(player.getName() + " ");
-        }
-        System.out.println();
-        return playersList;
-    }
-
-    /**
-     * Initializes the 6 tiles for each player at the start of the game.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void initTiles(Game game) {
-        for (int i = 0; i < game.getPlayers().length; i++) {
-            game.getPlayers()[i].refill();
-        }
-    }
-
-    /**
-     * Asks which player starts the game.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void startPlayer(Game game) {
-        System.out.println("Who is starting ? ");
-        game.setCurrentPlayer(robustReadingString());
-    }
-
-    /**
-     * Requests the necessary information to add the first tiles of the game.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void firstAdd(Game game) {
-        System.out.println("Start!");
-        displayPlayer(game);
-        game.first(direction(), indexes());
-        display(game.getGrid());
-    }
-
-    /**
-     * Gives one of the value of the direction enumeration to the requested direction.
-     *
-     * @return the direction.
-     */
-    public static Direction direction() {
-        System.out.print("Enter the direction of your tiles placement: ");
-        String direction = robustReadingDirection();
-        return switch (direction) {
-            case "d" -> Direction.DOWN;
-            case "u" -> Direction.UP;
-            case "l" -> Direction.LEFT;
-            case "r" -> Direction.RIGHT;
-            default -> throw new RuntimeException("invalid direction");
-        };
-    }
-
-    /**
-     * Requests the indexes of the tiles to add.
-     *
-     * @return an array of tiles indexes.
-     */
-    public static int[] indexes() {
-        Scanner keyboard = new Scanner(System.in);
-        List<Integer> indexes = new ArrayList<>();
-        boolean more = true;
-        while (indexes.size() < 6 && more) {
-            int index = robustReadingIndexes();
-            indexes.add(index);
-            System.out.println("Do you want to add more tiles ? (y or n)");
-            String yesOrNo = keyboard.nextLine();
-            if (yesOrNo.equals("n")) {
-                more = false;
-            }
-        }
-        int[] indexesTab = new int[indexes.size()];
-        for (int i = 0; i < indexesTab.length; i++) {
-            indexesTab[i] = indexes.get(i);
-        }
-        return indexesTab;
-    }
-
-    /**
-     * Asks what type of addition of tiles we want to make.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void add(Game game) {
-        displayPlayer(game);
-        System.out.print("Enter the type of play (o, l, m) : ");
-        String play = robustReadingString();
-        switch (play) {
-            case "o" -> playOneTile(game);
-            case "l" -> playLine(game);
-            case "m" -> playMultiple(game);
-        }
-        display(game.getGrid());
-    }
-
-    /**
-     * Requests the necessary information to add one tile.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void playOneTile(Game game){
-        int index = robustReadingIndexes();
-        System.out.print("Enter the row where you want to place the tile: ");
-        int row = robustReadingInt();
-        System.out.print("Enter the col where you want to place the tile: ");
-        int col = robustReadingInt();
-        game.play(row, col, index);
-    }
-
-    /**
-     * Requests the necessary information to add several aligned tiles.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void playLine(Game game){
-        System.out.print("Enter the row where you want to place the tile: ");
-        int row = robustReadingInt();
-        System.out.println("Enter the col where you want to place the tile: ");
-        int col = robustReadingInt();
-        game.play(row, col,direction(), indexes());
-    }
-
-    /**
-     * Requests the necessary information to add the multiple tiles.
-     *
-     * @param game the Qwirkle game.
-     */
-    public static void playMultiple(Game game){
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("How many tiles do you want to place ? ");
-        int nbTiles = robustReadingInt();
-        while(nbTiles<2 || nbTiles >6){
-            System.out.println("Number of tiles must be between 2 and 6, try again!");
-            nbTiles = robustReadingInt();
-        }
-        int[] playMultipleTiles = new int[nbTiles*3];
-        int indexTab = 0;
-        for(int i = 0; i<nbTiles; i++){
-            System.out.print("Enter the row where you want to place the tile " + (i+1) + ": ");
-            int row = robustReadingInt();
-            System.out.println("Enter the col where you want to place the tile: " + (i+1) + ": ");
-            int col = robustReadingInt();
-            int tileIndex = robustReadingIndexes();
-            playMultipleTiles[indexTab] = row;
-            playMultipleTiles[indexTab+1] = col;
-            playMultipleTiles[indexTab+2] = tileIndex;
-            indexTab = indexTab+3;
-        }
-        game.play(playMultipleTiles);
     }
 
     /**
      * Displays a game over message.
      */
-    public static void endGame(){
-        System.out.println("Bye Bye! Hope you liked it!");
+    public static void endGame() {
+        System.out.println();
+        System.out.println(ANSI_CYAN + "Bye Bye! Hope you liked it!" + ANSI_RESET);
     }
 }
