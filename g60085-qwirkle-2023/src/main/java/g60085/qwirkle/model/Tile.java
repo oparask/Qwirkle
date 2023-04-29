f6a43c1984757c546ceaac20f3cbddb2d7386202
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-29 22:07:34 +0200

    supression de la methode ToString

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Tile.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/Tile.java
@@ -4,14 +4,4 @@ package g60085.qwirkle.model;
  * Tile represents a tile in the game.
  */
 public record Tile(Color color, Shape shape) {
-    /**
-     * Allows to see the shape and color of a tile as text.
-     *
-     * @return a string representation of the object Tile.
-     */
-    @Override
-    public String toString() {
-        return this.shape.name() + ":" + this.color.name();
-    }
-
 }
