369a50795b4223c21550921560695ae58aa1c89c
60085@etu.he2b.be <60085@etu.he2b.be>
2023-04-29 22:08:30 +0200

    javadoc

--- g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/QwirkleException.java
+++ g60085-qwirkle-2023/src/main/java/g60085/qwirkle/model/QwirkleException.java
@@ -1,7 +1,12 @@
 package g60085.qwirkle.model;
 
-public class QwirkleException extends RuntimeException{
-
+/**
+ * As soon as an add request does not respect the Qwirkle game rules, an exception is thrown;
+ */
+public class QwirkleException extends RuntimeException {
+    /**
+     * @param message Message that appears when throwing the Qwirkle exception;
+     */
     public QwirkleException(String message) {
         super(message);
     }
